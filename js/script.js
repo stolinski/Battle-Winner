40231748afc9261f29f29eed0b9eb61f97680110
Scott Tolinski <scott.tolinski@gmail.com>
2012-06-29 21:30:43 -0400

    Now Actually Working - Not pretty yet.

--- js/script.js
+++ js/script.js
@@ -6,7 +6,16 @@ Scott Tolinski
 $(document).ready(function() {
 	var crew1,
 	crew2,
+	winner,
+	win,
+	fnd,
+	org,
+	btl,
+	dyn,
+	exe,
+	winz,
 	c = 0;
+	// Sets Crew Names to categories 
 	$('.crew1-name').focusout(function(){
 		crew1 = $('.crew1-name').val();
 		$('.radio1 span').html(crew1)
@@ -15,31 +24,67 @@ $(document).ready(function() {
 		crew2 = $('.crew2-name').val();
 		$('.radio2 span').html(crew2)
 	})
+	// Runs The show
 	$('.go').click(function(){
+		fnd = $('input:radio[name=fnd]:checked').val();
+		org = $('input:radio[name=org]:checked').val();
+		btl = $('input:radio[name=btl]:checked').val();
+		dyn = $('input:radio[name=dyn]:checked').val();
+		exe = $('input:radio[name=exe]:checked').val();
+		winz = $('input:radio[name=win]:checked').val();
 		crew1 = $('.crew1-name').val();
 		crew2 = $('.crew2-name').val();
-		$('#form-stuff').fadeOut("slow", function(){
+		$('#form-stuff').animate({
+			left: '100%'}, 1000, function(){
 			c++;
-			console.log(f);
 		});
 	});
+	// Advances Upon Clicking
 	$('html').click(function(){
 		console.log(c);
-		if (c === 0) {
-			return;
-		} else if (c === 1) {
-			var win = $('input:radio[name=optionsRadios]:checked').val();
-			checkWinner(win);
-			c++;
+		switch (c) {
+			case 0:
+				break;
+			case 1:
+				animateWin(fnd, "Foundation");
+				break;
+			case 2:
+				animateWin(org, "Originality");
+				break;
+			case 3:
+				animateWin(btl, "Battle");
+				break;
+			case 4:
+				animateWin(dyn, "Dynamics");
+				break;
+			case 5:
+				animateWin(exe, "Execution");
+				break;
+			case 6:
+				animateWin(winz, "the Battle");
+				break;
 		}
 	});
+
+
+	// Runs Animation
+	function animateWin(round, rnd) {
+		win = round;
+		winner = checkWinner(win);
+		$('#main').html('<h2>The Winner of ' + rnd + ' is:</h2><h1 class="winner">' + winner + '</h1>').hide().fadeIn("slow");
+		c++;		
+	}
+
+
+	// Compares fields
 	function checkWinner(win) {
 		console.log(win);
-
 		if (win == 'crew1') {
-			$('#main').html('<h1 class="winner">' + crew1 + '</h1>').hide().fadeIn("slow");	       
+			return crew1;
+		} else if (win == 'crew1') {
+			return crew2;
 		} else {
-			$('#main').html('<h1 class="winner">' + crew2 + '</h1>').hide().fadeIn("slow");
+			return "tie";
 		}
 	}
 });
